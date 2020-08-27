2e89e8e83c8de3389ce39ab9a8b9816e8af57acc
a150238 <eivind.askestad@nav.no>
2020-08-27 13:11:57 +0200

    Added liveness tests

--- src/main/kotlin/no/nav/pgi/skatt/leshendelse/Liveness.kt
+++ src/main/kotlin/no/nav/pgi/skatt/leshendelse/Liveness.kt
@@ -1,13 +1,12 @@
 package no.nav.pgi.skatt.leshendelse
 
 import io.ktor.application.*
-import io.ktor.application.Application
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
-const val IS_ALIVE_PATH = "/isAlive"
-const val IS_READY_PATH = "/isReady"
+internal const val IS_ALIVE_PATH = "/isAlive"
+internal const val IS_READY_PATH = "/isReady"
 
 internal fun Application.liveness() {
     routing {
--- src/test/kotlin/no/nav/pgi/skatt/leshendelse/Test.kt
+++ src/test/kotlin/no/nav/pgi/skatt/leshendelse/Test.kt
@@ -1,13 +1,49 @@
 package no.nav.pgi.skatt.leshendelse
 
-import org.junit.jupiter.api.Assertions.assertTrue
+import io.ktor.http.*
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers.ofString
 
-class Test {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal object Test {
+
+    private const val SERVER_PORT = 8080
+    private const val HOST = "http://localhost:$SERVER_PORT"
+    private val application = createApplication()
+    private val client = HttpClient.newHttpClient()
+
+    @BeforeAll
+    fun init() {
+        application.start()
+    }
+
+    @AfterAll
+    fun teardown() {
+        application.stop(100, 100)
+    }
 
     @Test
-    fun test() {
-        assertTrue(true)
+    fun isAlive() {
+        val response = client.send(createGetRequest(IS_ALIVE_PATH), ofString())
+        assertEquals(HttpStatusCode.OK.value, response.statusCode())
     }
+
+    @Test
+    fun isReady() {
+        val response = client.send(createGetRequest(IS_READY_PATH), ofString())
+        assertEquals(HttpStatusCode.OK.value, response.statusCode())
+    }
+
+    private fun createGetRequest(path: String): HttpRequest = HttpRequest.newBuilder()
+            .uri(URI.create("$HOST/$path"))
+            .GET()
+            .build()
 
 }
