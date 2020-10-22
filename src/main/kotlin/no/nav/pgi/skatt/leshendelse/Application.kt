c00f638610789b17fc2287980bd38e79ac761ab5
a150238 <eivind.askestad@nav.no>
2020-10-22 16:06:40 +0200

    Temporary fix to schema registry credential bug is fixed by nais team

--- src/main/kotlin/no/nav/pgi/skatt/leshendelse/Application.kt
+++ src/main/kotlin/no/nav/pgi/skatt/leshendelse/Application.kt
@@ -2,14 +2,18 @@ package no.nav.pgi.skatt.leshendelse
 
 import no.nav.pensjon.samhandling.naisserver.naisServer
 import no.nav.pgi.skatt.leshendelse.kafka.KafkaConfig
+import no.nav.pgi.skatt.leshendelse.skatt.FirstSekvensnummerClient
+import org.slf4j.LoggerFactory
 
+private val LOGGER = LoggerFactory.getLogger(Application::class.java)
 
 fun main() {
     val application = Application()
     try {
-        //application.startHendelseSkattLoop(KafkaConfig(), System.getenv())
-    } catch (e: Exception) {
-        application.stopServer()
+        application.startHendelseSkattLoop(KafkaConfig(), System.getenv())
+    } catch (e: Throwable) {
+        LOGGER.info(e.javaClass.name)
+        //application.stopServer() TODO vent til kafka-gjengen har fiksa greiene sine
     }
 }
 
