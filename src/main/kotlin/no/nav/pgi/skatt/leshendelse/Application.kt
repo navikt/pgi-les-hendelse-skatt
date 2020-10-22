111e52f2270730be3d911956717b70602a919ceb
a150238 <eivind.askestad@nav.no>
2020-10-22 15:01:58 +0200

    Using nais-client

--- src/main/kotlin/no/nav/pgi/skatt/leshendelse/Application.kt
+++ src/main/kotlin/no/nav/pgi/skatt/leshendelse/Application.kt
@@ -1,21 +1,20 @@
 package no.nav.pgi.skatt.leshendelse
 
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import no.nav.pensjon.samhandling.liveness.isAlive
-import no.nav.pensjon.samhandling.liveness.isReady
-import no.nav.pensjon.samhandling.metrics.metrics
+import no.nav.pensjon.samhandling.naisserver.naisServer
 import no.nav.pgi.skatt.leshendelse.kafka.KafkaConfig
 
 
 fun main() {
     val application = Application()
-    application.startHendelseSkattLoop(KafkaConfig(), System.getenv())
+    try {
+        application.startHendelseSkattLoop(KafkaConfig(), System.getenv())
+    } catch (e: Exception) {
+        application.stopServer()
+    }
 }
 
 internal class Application {
-
-    private val naisServer = embeddedServer(Netty, createApplicationEnvironment())
+    private val naisServer = naisServer()
 
     init {
         naisServer.start()
@@ -24,15 +23,6 @@ internal class Application {
     internal fun startHendelseSkattLoop(kafkaConfig: KafkaConfig, env: Map<String, String>, loopForever: Boolean = true) =
             hendelseSkattLoop(kafkaConfig, env, loopForever)
 
-    private fun createApplicationEnvironment(serverPort: Int = 8080) =
-            applicationEngineEnvironment {
-                connector { port = serverPort }
-                module {
-                    isAlive()
-                    isReady()
-                    metrics()
-                }
-            }
 
     internal fun stopServer() {
         naisServer.stop(100, 100)
