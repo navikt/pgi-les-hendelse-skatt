3ecd89cc431daac7b867e051360fff0f80afa491
a150238 <eivind.askestad@nav.no>
2020-11-30 18:01:20 +0100

    Lazy init of first sekvensnummer

--- src/main/kotlin/no/nav/pgi/skatt/leshendelse/Sekvensnummer.kt
+++ src/main/kotlin/no/nav/pgi/skatt/leshendelse/Sekvensnummer.kt
@@ -9,13 +9,12 @@ import org.slf4j.LoggerFactory
 private val LOG = LoggerFactory.getLogger(Sekvensnummer::class.java.simpleName)
 
 internal class Sekvensnummer(kafkaFactory: KafkaFactory, env: Map<String, String>) {
-    private val sekvensnummerConsumer = SekvensnummerConsumer(kafkaFactory)
     private val nextSekvensnummerProducer = SekvensnummerProducer(kafkaFactory)
-    private val firstSekvensnummerClient = FirstSekvensnummerClient(env)
+    private val firstSekvensnummer: Long by lazy { getInitialSekvensnummer(kafkaFactory, env) }
     private var currentSekvensnummer = NOT_INITIALIZED
 
     internal fun getSekvensnummer(): Long {
-        if (currentSekvensnummer == NOT_INITIALIZED) setSekvensnummer(getInitialSekvensnummer())
+        if (currentSekvensnummer == NOT_INITIALIZED) setSekvensnummer(firstSekvensnummer)
         return currentSekvensnummer
     }
 
@@ -34,16 +33,11 @@ internal class Sekvensnummer(kafkaFactory: KafkaFactory, env: Map<String, String
         }
     }
 
-
-    private fun getInitialSekvensnummer(): Long = sekvensnummerConsumer.getNextSekvensnummer()?.toLong()
-            ?: firstSekvensnummerClient.getFirstSekvensnummer()
-
     internal fun addSekvensnummerToTopic(sekvensnummer: Long, synchronous: Boolean = false) {
         nextSekvensnummerProducer.writeSekvensnummer(sekvensnummer, synchronous)
     }
 
     internal fun close() {
-        sekvensnummerConsumer.close()
         nextSekvensnummerProducer.close()
     }
 
@@ -51,4 +45,12 @@ internal class Sekvensnummer(kafkaFactory: KafkaFactory, env: Map<String, String
         internal const val NOT_INITIALIZED = -9999L
         internal const val USE_PREVIOUS = -1L
     }
+}
+
+private fun getInitialSekvensnummer(kafkaFactory: KafkaFactory, env: Map<String, String>): Long {
+    val consumer = SekvensnummerConsumer(kafkaFactory)
+    val client = FirstSekvensnummerClient(env)
+    val sekvensnummer = consumer.getNextSekvensnummer()?.toLong() ?: client.getFirstSekvensnummer()
+    consumer.close()
+    return sekvensnummer
 }
