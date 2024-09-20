c7ff7094ccc028e4800428b48a5c7c2b4583179f
Jan Kroken <jan.kroken@nav.no>
2024-09-20 09:32:34 +0200

    la på litt midlertidig debug-logging i Counters

--- src/main/kotlin/no/nav/pgi/skatt/leshendelse/Counters.kt
+++ src/main/kotlin/no/nav/pgi/skatt/leshendelse/Counters.kt
@@ -2,6 +2,7 @@ package no.nav.pgi.skatt.leshendelse
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
+import org.slf4j.LoggerFactory
 import java.util.concurrent.atomic.AtomicLong
 
 class Counters(private val meterRegistry: MeterRegistry) {
@@ -33,18 +34,27 @@ class Counters(private val meterRegistry: MeterRegistry) {
     )
 
     fun incrementHendelserTopTopic(count: Int) {
+        log.info("Counters.incrementHendelserTopTopic:$count")
         hendelserToTopic.increment(count.toDouble())
     }
 
     fun incrementFailedToTopic(count: Int) {
+        log.info("Counters.incrementFailedToTopic:$count")
         hendelserFailedToTopic.increment(count.toDouble())
     }
 
     fun incrementPolledFromSkatt(count: Int) {
+        log.info("Counters.incrementPolledFromSkattTopic:$count")
         polledFromSkattCounter.increment(count.toDouble())
     }
 
     fun setPersistredSekvensnummer(sekvensnummer: Long) {
+        log.info("Counters.setPersistredSekvensnummer:$sekvensnummer")
         persistedSekvensnummer.set(sekvensnummer)
     }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(Counters::class.java)
+    }
+
 }
